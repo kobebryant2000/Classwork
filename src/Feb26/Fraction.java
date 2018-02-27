5a14e9bb1abbeea66914620b2be9fd5c89dea4ee
kobebryant2000 <sportsalex646@gmail.com>
2018-02-27 09:17:58 -0600

    Tested our compareTo method in the runner

--- src/Feb26/Fraction.java
+++ src/Feb26/Fraction.java
@@ -3,12 +3,12 @@ package Feb26;
 public class Fraction implements Comparable<Fraction>
 {
     private int num;
-    private int denom;
-
+    private int deno
     public Fraction(int num, int denom)
     {
         this.num = num;
-        this.denom = denom;
+        this.denom = dm;
+        enom;
         simplify();
 
     }
@@ -18,5 +18,9 @@ public class Fraction implements Comparable<Fraction>
         if (difference > 0) return 1;
         else if (difference < 0) return - 1;
         return 0;
+    }
+    private void simplify()
+    {
+
     }
 }
---
+++ src/Feb26/FractionRunner.java
@@ -0,0 +1,12 @@
+package Feb26;
+
+public class FractionRunner
+
+{
+    public static void main (String[] args)
+    {
+        Fraction f1 = new Fraction (1,4);
+        Fraction f2 = new Fraction (1,3);
+        System.out.println(f1.compareTo(f2));
+    }
+}
